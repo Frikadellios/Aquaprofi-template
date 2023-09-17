2adc5292a8ea10ac870c9fc6a3647e712ea9e1b7
Hrihorii Ilin <devopsick@pm.me>
2023-09-17 13:13:34 +0000

    + node

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,18 @@ import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
-
 import netlify from "@astrojs/netlify/functions";
+
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind({configFile: './tailwind.config.cjs'}), sitemap()],
+  integrations: [react(), tailwind({
+    configFile: './tailwind.config.cjs'
+  }), sitemap()],
   site: 'https://aquaprofi.com',
   output: "server",
-  adapter: netlify({
-    dist: new URL('./dist/', import.meta.url),
-    builders: true,
+  adapter: node({
+    mode: "standalone"
   })
 });
