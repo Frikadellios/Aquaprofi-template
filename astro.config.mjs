54ccc5270aec447bf6e9b9f72290bb1262286758
Hrihorii Ilin <devopsick@pm.me>
2023-09-17 12:46:03 +0000

    + netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,17 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-
 import sitemap from "@astrojs/sitemap";
+
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind(), sitemap()],
   site: 'https://aquaprofi.com',
+  output: "server",
+  adapter: netlify({
+    dist: new URL('./dist/', import.meta.url),
+    builders: true,
+  })
 });
